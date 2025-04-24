4a0ccf1655354519c44048e4df74860cf0d1deac
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-04-24 16:53:12 -0300

    feat: adicionar classe Carteira com métodos de gerenciamento de transações - Implementados métodos para adicionar, exibir e filtrar transações - Adicionados métodos para calcular gastos totais e renda total

--- script.py
+++ script.py
@@ -8,3 +8,28 @@ class Transacao:
     def resumo(self):
         sinal = '+' if self.valor >= 0 else ''
         return f"{self.descricao} | {sinal}{self.valor} | {self.categoria} | {self.data}"
+    
+    
+class Carteira:
+    def __init__(self):
+        self.transacoes = []
+
+        def adicionar(self, transacao):
+            self.transacoes.append(transacao)
+
+        def exibir_transacoes(self):
+         for transacao in self.transacoes:
+                print(transacao.resumo())
+
+        def saldo(self):
+            return sum(transacao.valor for transacao in self.transacoes)
+        
+        def filtrar_por_categoria(self, categoria):
+            return[transacao for transacao in self.transacoes if transacao.categoria == categoria]
+        
+        def gastos_totais(self):
+            return sum(transacao.valor for transacao in self.transacoes if transacao.valor < 0)
+        
+        def renda_total(self):
+            return sum(transacao.valor for transacao in self.transacoes if transacao.valor > 0)
+        
